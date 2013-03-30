1252efc81240ceb193838a272e1f894cb9366136
Max Horbul <max@gorbul.net>
2013-03-29 19:25:05 -0700

    fix login method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Boxroom::Application.configure do
     :domain => ENV['SMTP_DOMAIN'],
     :user_name => ENV['SMTP_USERNAME'],
     :password => ENV['SMTP_PASSWORD'],
-    :authentication => 'plain',
+    :authentication => 'login',
     :enable_starttls_auto => true
   }
 
